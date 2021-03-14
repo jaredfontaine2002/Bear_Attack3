cfbce2e062d8a1407b436699658dabba83e9275f
Jared <jaredfontaine2002@gmail.com>
2021-03-14 13:23:10 -0700

    changes for pythonanywhere

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def update_graph(Slct_bear):
     return container, fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=False)
 
 
 # This is a sample Python script.
